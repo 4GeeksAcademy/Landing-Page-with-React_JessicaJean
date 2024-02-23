0db63a497346047e1523c1d425d44f9c02f77f8a
Jessica Jean <140281920+Jessjean17@users.noreply.github.com>
2024-02-23 21:45:22 +0000

    draft

---
+++ src/js/card.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Home from "./component/home";
+
+const Card =(
+    <div classname="row row-cols-1 row-cols-md-3 g-4">
+  <div classname="col">
+    <div classname="card">
+      <img src="..." classname="card-img-top" alt="..."></img>
+      <div classname="card-body">
+        <h5 classname="card-title">Card title</h5>
+        <p classname="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div classname="col">
+    <div classname="card">
+      <img src="..." classname="card-img-top" alt="..."></img>
+      <div classname="card-body">
+        <h5 classname="card-title">Card title</h5>
+        <p classname="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div classname="col">
+    <div classname="card">
+      <img src="..." classname="card-img-top" alt="..."></img>
+      <div classname="card-body">
+        <h5 classname="card-title">Card title</h5>
+        <p classname="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div classnameName="col">
+    <div classname="card">
+      <img src="..." classname="card-img-top" alt="..."></img>
+      <div classname="card-body">
+        <h5 classname="card-title">Card title</h5>
+        <p classname="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+);
+
+export default Card;
